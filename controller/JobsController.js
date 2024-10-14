d0845b9441062fe64de0893741b239a316af6b11
aakash kumar <aakashkumar722002@gmail.com>
2024-10-14 11:47:07 +0530

    updated

--- controller/JobsController.js
+++ controller/JobsController.js
@@ -48,6 +48,7 @@ const getJobs = async (req, res) => {
 const getJobById = async (req, res) => {
     const jobId = req.params.id;
 
+
     try {
         const job = await Job.findById(jobId);
         if (!job) {
